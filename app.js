5d2b9d00f3b24dd288932dd4230c188b3e689b4a
Bailey Dalton <baileydalton007@gmail.com>
2020-03-24 21:51:19 -0400

    Adds the naming of points

--- app.js
+++ app.js
@@ -32,21 +32,22 @@ function resize() {
   var currentScreenRatio = winWidth / winHeight;
   var optimalRatio = Math.min(scaleToFitX, scaleToFitY);
   if (currentScreenRatio >= 1.77 && currentScreenRatio <= 1.79) {
-    canvas.style.width = winWidth + "px";
-    canvas.style.height = winHeight + "px";
-    renderer.width = canvas.style.width;
-    renderer.height = canvas.style.height;
+	canvas.style.width = winWidth + "px";
+	canvas.style.height = winHeight + "px";
+	renderer.width = canvas.style.width;
+	renderer.height = canvas.style.height;
   } else {
-    canvas.style.width = 800 * optimalRatio + "px";
-    canvas.style.height = 480 * optimalRatio + "px";
-    renderer.width = canvas.style.width;
-    renderer.height = canvas.style.height;
+	canvas.style.width = 800 * optimalRatio + "px";
+	canvas.style.height = 480 * optimalRatio + "px";
+	renderer.width = canvas.style.width;
+	renderer.height = canvas.style.height;
   }
 }
 
 window.addEventListener("resize", resize, false);
 
 let dotArr = [];
+let namedDotsArr = [];
 let selectedDotIndex;
 
 const createDot = () => {
@@ -61,11 +62,11 @@ const createDot = () => {
   dot.buttonMode = true;
   dot.anchor.set(0.5);
   dot
-    .on("pointerdown", onDotDragStart)
-    .on("pointerup", onDragEnd)
-    .on("pointerupoutside", onDragEnd)
-    .on("pointermove", onDragMove)
-    .on("rightdown", rightClick);
+	.on("pointerdown", onDotDragStart)
+	.on("pointerup", onDragEnd)
+	.on("pointerupoutside", onDragEnd)
+	.on("pointermove", onDragMove)
+	.on("rightdown", rightClick);
 
   board.addChild(dot);
   dotArr.push(dot);
@@ -83,6 +84,11 @@ function onDotDragStart(event) {
   this.alpha = 0.5;
   this.dragging = true;
   selectedDotIndex = this.index;
+  if (naming == true && namedDotsArr.includes(this) == false) {
+	namedDotsArr.push(this);
+	namePoint(this.index);
+	naming = false;
+  }
 }
 
 function onDragStart(event) {
@@ -104,9 +110,9 @@ function onDragEnd() {
 
 function onDragMove() {
   if (this.dragging) {
-    const newPosition = this.data.getLocalPosition(this.parent);
-    this.x = newPosition.x;
-    this.y = newPosition.y;
+	const newPosition = this.data.getLocalPosition(this.parent);
+	this.x = newPosition.x;
+	this.y = newPosition.y;
   }
 }
 
@@ -116,9 +122,9 @@ let lineArr = [];
 
 const createLine = (startDotIndex, endDotIndex) => {
   const line = {
-    index: lineArr.length,
-    startDotIndex: startDotIndex,
-    endDotIndex: endDotIndex
+	index: lineArr.length,
+	startDotIndex: startDotIndex,
+	endDotIndex: endDotIndex
   };
   lineArr.push(line);
 };
@@ -127,16 +133,18 @@ const drawLine = (startDotIndex, endDotIndex) => {
   let startDot = dotArr[startDotIndex];
   let endDot = dotArr[endDotIndex];
   graph
-    .lineStyle(8, 0xf4a0f4)
-    .moveTo(startDot.x, startDot.y)
-    .lineTo(endDot.x, endDot.y);
+	.lineStyle(8, 0xf4a0f4)
+	.moveTo(startDot.x, startDot.y)
+	.lineTo(endDot.x, endDot.y);
 };
 
 const clearBoard = () => {
   for (let i = board.children.length - 1; i >= 0; i--) {
-    board.removeChild(board.children[i]);
+	board.removeChild(board.children[i]);
   }
   dotArr = [];
+  namedDotsArr = [];
+  nameArr = [];
   lineArr = [];
   textArr = [];
   tPointArr = [];
@@ -158,7 +166,6 @@ const drawText = () => {
   //input.wordWrapWidth = 40,
   //input.width = input.text * 5; + 100
   input.caretColor = 0xffffff;
-  console.log(input.width);
   drawPickPoint(input.x + input.width / 2 + 65, input.y + input.height / 2);
   input.on("pointerdown", drawPickPoint);
   textArr.push(input);
@@ -177,22 +184,22 @@ const drawPickPoint = (x, y) => {
   dot.scale.set(0.03, 0.03);
   dot.anchor.set(0.5);
   dot
-    .on("pointerdown", onDragStart)
-    .on("pointerup", onDragEnd)
-    .on("pointerupoutside", onDragEnd)
-    .on("pointermove", onDragMove)
-    .on("rightdown", rightClick);
+	.on("pointerdown", onDragStart)
+	.on("pointerup", onDragEnd)
+	.on("pointerupoutside", onDragEnd)
+	.on("pointermove", onDragMove)
+	.on("rightdown", rightClick);
   tPointArr.push(dot);
   board.addChild(dot);
 };
 
 const updateText = () => {
   if (textArr.length > 0) {
-    for (let i = 0; i < textArr.length; i++) {
-      textArr[i].width = textArr[i].text.length * 15 + 20;
-      textArr[i].x = tPointArr[i].x;
-      textArr[i].y = tPointArr[i].y;
-    }
+	for (let i = 0; i < textArr.length; i++) {
+	  textArr[i].width = textArr[i].text.length * 15 + 20;
+	  textArr[i].x = tPointArr[i].x;
+	  textArr[i].y = tPointArr[i].y;
+	}
   }
 };
 
@@ -200,14 +207,41 @@ let wait = false;
 const takeScreenshot = () => {
   wait = true;
   renderer.renderer.extract.canvas(renderer.stage).toBlob(b => {
-    const a = document.createElement("a");
-    document.body.append(a);
-    a.download = "screenshot";
-    a.href = URL.createObjectURL(b);
-    a.click();
-    a.remove();
+	const a = document.createElement("a");
+	document.body.append(a);
+	a.download = "screenshot";
+	a.href = URL.createObjectURL(b);
+	a.click();
+	a.remove();
   }, "image/png");
 };
+
+let naming = false;
+let nameArr = [];
+
+const startNaming = () => {naming = true};
+
+const namePoint = (dotIndex) => {
+	let input = new PixiTextInput("text", style);
+	input.width = 400;
+	input.background = false;
+	input.position.x = 100;
+  	input.position.y = 250;
+	input.caretColor = 0xffffff;
+	nameArr.push(input);
+  	board.addChild(input);
+
+};
+
+const updatePointNames = () => {
+	if (nameArr.length > 0) {
+	  for (let i = 0; i < nameArr.length; i++) {
+		nameArr[i].width = nameArr[i].text.length * 15 + 20;
+		nameArr[i].x = namedDotsArr[i].x;
+		nameArr[i].y = namedDotsArr[i].y - 40;
+	  }
+	}
+  };
 
 const drawDotButton = () => {
   const texture = PIXI.Texture.from("./assets/addDot.png");
@@ -274,6 +308,20 @@ const drawScreenShotButton = () => {
   menuCon.addChild(button);
 };
 
+const drawNameButton = () => {
+	const texture = PIXI.Texture.from("./assets/name.png");
+	const button = new PIXI.Sprite(texture);
+	button.x = windowWidth - 80;
+	button.y = 240;
+	button.scale.set(0.1, 0.1);
+	button.interactive = true;
+	button.buttonMode = true;
+	button.anchor.set(0.5);
+	button.on("pointerdown", startNaming);
+	menuCon.addChild(button);
+  };
+
+
 const drawMenuButton = () => {
   const texture = PIXI.Texture.from("./assets/menu.png");
   const button = new PIXI.Sprite(texture);
@@ -305,48 +353,48 @@ const drawOptionButton = () => {
 let isOptionsOpen = false;
 const openOptions = () => {
   if (isOptionsOpen == false) {
-    isOptionsOpen = true;
-    if (isMenuOpen == true) {
-      openMenu();
-    }
-    const texture = PIXI.Texture.from("./assets/uiBackground.png");
-    const ui = new PIXI.Sprite(texture);
-    ui.x = windowWidth - 200;
-    ui.y = 0;
-    ui.scale.set(1, 3);
-    ui.interactive = true;
-    ui.buttonMode = false;
-    //ui.anchor.set(0.5);
-    ui.zIndex = -1;
-    optionsCon.addChild(ui);
-    renderer.stage.addChild(optionsCon);
+	isOptionsOpen = true;
+	if (isMenuOpen == true) {
+	  openMenu();
+	}
+	const texture = PIXI.Texture.from("./assets/uiBackground.png");
+	const ui = new PIXI.Sprite(texture);
+	ui.x = windowWidth - 200;
+	ui.y = 0;
+	ui.scale.set(1, 3);
+	ui.interactive = true;
+	ui.buttonMode = false;
+	//ui.anchor.set(0.5);
+	ui.zIndex = -1;
+	optionsCon.addChild(ui);
+	renderer.stage.addChild(optionsCon);
   } else {
-    renderer.stage.removeChild(optionsCon);
-    isOptionsOpen = false;
+	renderer.stage.removeChild(optionsCon);
+	isOptionsOpen = false;
   }
 };
 
 let isMenuOpen = false;
 const openMenu = () => {
   if (isMenuOpen == false) {
-    isMenuOpen = true;
-    if (isOptionsOpen == true) {
-      openOptions();
-    }
-    const texture = PIXI.Texture.from("./assets/uiBackground.png");
-    const ui = new PIXI.Sprite(texture);
-    ui.x = windowWidth - 200;
-    ui.y = 0;
-    ui.scale.set(1, 3);
-    ui.interactive = true;
-    ui.buttonMode = false;
-    //ui.anchor.set(0.5);
-    ui.zIndex = -1;
-    menuCon.addChild(ui);
-    renderer.stage.addChild(menuCon);
+	isMenuOpen = true;
+	if (isOptionsOpen == true) {
+	  openOptions();
+	}
+	const texture = PIXI.Texture.from("./assets/uiBackground.png");
+	const ui = new PIXI.Sprite(texture);
+	ui.x = windowWidth - 200;
+	ui.y = 0;
+	ui.scale.set(1, 3);
+	ui.interactive = true;
+	ui.buttonMode = false;
+	//ui.anchor.set(0.5);
+	ui.zIndex = -1;
+	menuCon.addChild(ui);
+	renderer.stage.addChild(menuCon);
   } else {
-    renderer.stage.removeChild(menuCon);
-    isMenuOpen = false;
+	renderer.stage.removeChild(menuCon);
+	isMenuOpen = false;
   }
 };
 
@@ -363,15 +411,15 @@ const makeSelecting = () => {
 
 const selectDots = () => {
   if (selecting) {
-    if (selectedDotIndex !== undefined) {
-      if (startP == undefined) {
-        startP = selectedDotIndex;
-      } else if (endP == undefined && selectedDotIndex !== startP) {
-        endP = selectedDotIndex;
-        createLine(startP, endP);
-        selecting = false;
-      }
-    }
+	if (selectedDotIndex !== undefined) {
+	  if (startP == undefined) {
+		startP = selectedDotIndex;
+	  } else if (endP == undefined && selectedDotIndex !== startP) {
+		endP = selectedDotIndex;
+		createLine(startP, endP);
+		selecting = false;
+	  }
+	}
   }
 };
 
@@ -381,6 +429,7 @@ const init = () => {
   drawTrashButton();
   drawTextButton();
   drawScreenShotButton();
+  drawNameButton();
   drawMenuButton();
   drawOptionButton();
 };
@@ -388,7 +437,7 @@ const init = () => {
 const updateLine = () => {
   graph.clear();
   for (let i = 0; i < lineArr.length; i++) {
-    drawLine(lineArr[i].startDotIndex, lineArr[i].endDotIndex);
+	drawLine(lineArr[i].startDotIndex, lineArr[i].endDotIndex);
   }
 };
 
@@ -403,4 +452,5 @@ renderer.ticker.add(function(delta) {
   selectDots();
   updateLine();
   updateText();
+  updatePointNames();
 });
